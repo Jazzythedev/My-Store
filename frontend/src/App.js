8c979a1489ec1a1381958ce7fa68e081e8a8fe6f
Jazzythedev <98485836+Jazzythedev@users.noreply.github.com>
2022-03-07 20:30:18 -0800

    Header and Footer components linked to app.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,14 @@
+import Footer from "./components/Footer";
+import Header from "./components/Header";
 
 const App = () => {
   return (
     <>
+      <Header />
+      <main>
       <h1> Welcome to MyShop!</h1>
+      <Footer />
+      </main>
     </>
   );
 }
