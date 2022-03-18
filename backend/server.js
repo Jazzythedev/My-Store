07370fd77c30e6e87976a29689f9f2a2a7dbd6ab
Jazzythedev <98485836+Jazzythedev@users.noreply.github.com>
2022-03-17 19:50:13 -0700

    instruct app to open up and read .env file

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,9 @@
 import express from 'express'                      /* import express to create routes, syntax changed fom common js to emca  */
 import products from './data/products.js'               /* import js file to be used. must add .js, syntax changed fom common js to emca  */
-
+import dotenv from 'dotenv'
 
 const app = express()                                                      /*  create instance of express to setup routes ie using the bus */
+dotenv.config()                                                            /* tells code to open the env file and read it */
 
  app.get('/api/products', (req, res) => {                                      /* 'app' is express and it is the listener .client calling API using get route. 'get' is one of many routes */
     res.json(products)                                                         /*  send req, respond back with json of products */
@@ -15,6 +16,7 @@ const app = express()                                                      /*  c
 
 })                               
 
-app.listen(5000, console.log('Server is running on port 5000'));                            /* define port to run bkend 5000, express is listener. listen on port # and if you are successful to listen, write the following message */
+const PORT = process.env.PORT || 5000                                             /* create a variable called PORT. THis reads off the environment variable assigned a port or goes to default port which is 5000. "in current process look for the env variable called PORT" */
+app.listen(PORT, console.log(`Server is running on port ${PORT}`));                            /* define port to run bkend 5000, express is listener. listen on port # and if you are successful to listen, write the following message. {changed to PORT to represent the above, reading off the .env file for a port number} */
 
  
