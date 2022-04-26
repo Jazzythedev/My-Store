03ef281b3fdb72edc49b2aa01362ed3b00aed206
Jazzythedev <98485836+Jazzythedev@users.noreply.github.com>
2022-04-26 07:06:45 -0700

    prepared to deploy

--- backend/server.js
+++ backend/server.js
@@ -5,18 +5,13 @@ import userRoutes from './routes/userRoutes.js'                                 
 import orderRoutes from './routes/orderRoutes.js'
 import productRoutes from './routes/productRoutes.js'                               /* import these because that is where function to fire routes to products and details are stored, an we can refer to it */
 import { notFound, errorHandler } from './middleware/errorMiddleware.js'              /* import it here so you can use it for the whole app. */
-
+import path from 'path'
 
 const app = express()                                                      /*  create instance of express to setup routes ie using the bus */
 dotenv.config()                                                             /* tells code to open the env file and read it */
  connectDB()                                                                         /* execute function to connect to DB */
                                                       
  /* server is entry point so routs can be called from here */         
-
- app.get('/', (req, res) => {
-   res.json({'msg': 'Hello World'})
-
- })
  
   app.use(express.json())                                       // components are in html. we want to make sure all  post data that we recueve is in form of a json. in server.js when express gets data convert it into json using app.use(express.json()). since its called from here anytime you are routing and reading post data it will parse it and return it in form of json,
   app.use('/api/products', productRoutes)                         /* telling express if you see a a url that says api/products, route them to productRoutes  */
@@ -28,10 +23,23 @@ dotenv.config()                                                             /* t
     res.send(process.env.PAYPAL_CLIENT_ID)
   )
 
-  app.use(notFound)                                                   /* notFound funtion */
-  app.use(errorHandler)                                               /* this is the eror middleware handler that handles exceptions for the entire application if there isnt a trycatch. this is a route, but we dont specify a route url bc we want express to use this for every call that is made. so that if there is an unhandled route it will be diverted to the middleware. */
+  
+  const __dirname = path.resolve()
 
+if (process.env.NODE_ENV === 'production'){
+  app.use(express.static(path.join(__dirname, '/frontend/build')))
 
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
+  })
+}else {
+  app.get('/', (req, res) => {
+    res.send('API is running....')
+  })
+}
+
+app.use(notFound)                                                   /* notFound funtion */
+app.use(errorHandler)                                               /* this is the eror middleware handler that handles exceptions for the entire application if there isnt a trycatch. this is a route, but we dont specify a route url bc we want express to use this for every call that is made. so that if there is an unhandled route it will be diverted to the middleware. */
 
 
 const PORT = process.env.PORT || 5000                                             /* create a variable called PORT. THis reads off the environment variable assigned a port or goes to default port which is 5000. "in current process look for the env variable called PORT" */
